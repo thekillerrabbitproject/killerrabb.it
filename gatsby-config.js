7cda193330ae48992e34ed53c22cd20397be997c
Pedro Costa Neves <costa.neves@me.com>
2019-07-16 22:55:19 +0200

    Add graphql api

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,12 @@
+const fs = require("fs")
+const { buildSchema, buildClientSchema } = require("graphql")
+
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `The Killer-Rabbit Photography`,
+    description: ``,
+    author: `@persocon`,
+    siteUrl: 'https://killerrabb.it',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -15,20 +19,18 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
-      },
-    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-source-graphql`,
+      options: {
+        fieldName: `api`,
+        url: TKRP_GRAPHQL_API,
+        typeName: `persocon`,
+      },
+    },
+    `gatsby-plugin-sass`,
+    `gatsby-plugin-offline`,
   ],
 }
