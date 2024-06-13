1b6685d8c4314fb7edb7fa399057a0179b8602a4
Pedro Costa Neves <pedro.costaneves@adac-camping.de>
2024-06-13 18:16:31 +0200

    Fix poster

--- src/components/Video/index.js
+++ src/components/Video/index.js
@@ -1,21 +1,27 @@
 import styles from '@/css/VideoComponent.module.css';
 import { video } from '@/types/index';
-import { getVideoName } from '@/utils';
+import { getImageLocalSrc, getVideoName } from '@/utils';
 
-const Video = async ({ data }) => (
-  <video
-    className={styles.video}
-    controls
-    src={getVideoName(data.slug, data.videoThingy.featuredVideo.mediaItemUrl)}
-    poster={data.featuredImage.node.sourceUrl}
-    controlsList="nodownload noplaybackrate"
-    disablePictureInPicture
-    preload="metadata"
-    title={data.title}
-  >
-    Sorry, your browser doesn&rsquo;t support embedded videos
-  </video>
-);
+const Video = async ({ data }) => {
+  const { jpg: poster } = getImageLocalSrc({
+    sourceUrl: data.featuredImage.node.sourceUrl,
+  });
+
+  return (
+    <video
+      className={styles.video}
+      controls
+      src={getVideoName(data.slug, data.videoThingy.featuredVideo.mediaItemUrl)}
+      poster={poster}
+      controlsList="nodownload noplaybackrate"
+      disablePictureInPicture
+      preload="metadata"
+      title={data.title}
+    >
+      Sorry, your browser doesn&rsquo;t support embedded videos
+    </video>
+  );
+};
 
 Video.propTypes = video;
 
