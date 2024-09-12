8ee4aa133a2e594858aa8fc0ee56e1e76feee49b
Pedro Costa Neves <pedro.costaneves@adac-camping.de>
2024-09-12 14:11:21 +0200

    Add onError link, add RetryLink

--- download-assets.mjs
+++ download-assets.mjs
@@ -1,14 +1,59 @@
 import pkg from '@apollo/client';
+import { onError } from '@apollo/client/link/error/index.js';
+import { RetryLink } from '@apollo/client/link/retry/index.js';
 
 import query from './src/graphql/Downloader/StaticAssets.mjs';
 import shareService from './src/utils/shareimage.mjs';
 
 import fs from 'fs-extra';
 
-const { ApolloClient, InMemoryCache } = pkg;
+const { ApolloClient, from, HttpLink, InMemoryCache } = pkg;
+
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  const queryAndVariables = {
+    query: print(operation.query),
+    variables: operation?.variables,
+  };
+
+  if (graphQLErrors) {
+    for (const error of graphQLErrors) {
+      console.error(
+        {
+          error,
+          ...queryAndVariables,
+        },
+        'errorLink GraphQL error',
+      );
+    }
+  }
+
+  if (networkError) {
+    console.error(
+      {
+        error: networkError,
+        ...queryAndVariables,
+      },
+      'errorLink Network error',
+    );
+  }
+});
+
+const retryLink = new RetryLink({
+  attempts: {
+    max: 2,
+  },
+  delay: {
+    initial: 20_000, // 20s secconds in milliseconds, it multiplies exponentialy
+    max: 60_000, // 1 minute in milliseconds
+  },
+});
+
+const httpLink = new HttpLink({
+  uri: 'https://www.tkrp.net/graphql',
+});
 
 const client = new ApolloClient({
-  uri: 'https://www.tkrp.net/graphql',
+  link: from([errorLink, retryLink, httpLink]),
   cache: new InMemoryCache({}),
 });
 
--- src/utils/apollo-client.js
+++ src/utils/apollo-client.js
@@ -1,9 +1,53 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, from, HttpLink, InMemoryCache } from '@apollo/client';
+import { onError } from '@apollo/client/link/error';
+import { RetryLink } from '@apollo/client/link/retry';
 
 import { removeScaled } from '@/utils';
 
-const client = new ApolloClient({
+const errorLink = onError(({ graphQLErrors, networkError, operation }) => {
+  const queryAndVariables = {
+    query: print(operation.query),
+    variables: operation?.variables,
+  };
+
+  if (graphQLErrors) {
+    for (const error of graphQLErrors) {
+      console.error(
+        {
+          error,
+          ...queryAndVariables,
+        },
+        'errorLink GraphQL error',
+      );
+    }
+  }
+
+  if (networkError) {
+    console.error(
+      {
+        error: networkError,
+        ...queryAndVariables,
+      },
+      'errorLink Network error',
+    );
+  }
+});
+
+const retryLink = new RetryLink({
+  attempts: {
+    max: 2,
+  },
+  delay: {
+    initial: 20_000, // 20s secconds in milliseconds, it multiplies exponentialy
+    max: 60_000, // 1 minute in milliseconds
+  },
+});
+
+const httpLink = new HttpLink({
   uri: 'https://www.tkrp.net/graphql',
+});
+
+const config = {
   cache: new InMemoryCache({
     typePolicies: {
       MediaItem: {
@@ -20,6 +64,11 @@ const client = new ApolloClient({
       },
     },
   }),
+};
+
+const client = new ApolloClient({
+  link: from([errorLink, retryLink, httpLink]),
+  ...config,
 });
 
 export default client;
